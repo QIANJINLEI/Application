7b59109094028acf770dbf10bb2c1e25f413553d
MR.QIAN <18914010426@163.com>
2016-12-15 00:42:52 +0800

    1.你好啊

--- app/src/main/java/com/demo/mrqian/application/MainActivity.java
+++ app/src/main/java/com/demo/mrqian/application/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //尼玛好啊
-
+        //我行好
     }
 
 }
